a0a0598625801fd13774884c264b8dea07b2167e
Preetish Ubhrani <preetishubhrani@Preetishs-MacBook-Pro.local>
2025-07-01 22:13:11 +0530

    Started with Top Bar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TopBar from "./components/TopBar"
+
+function App() {
+
+  return (
+    <>
+      <TopBar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,43 @@
+import '../styles/TopBar.css'
+
+export default function TopBar(){
+    return (
+        <>
+            <div className="topbar">
+                <div className="path">
+                    <div className="panel">
+                        <div className="three"></div>
+                        <div className="one"></div>
+                    </div>
+                    <div className="path-display">
+                        <span>Workspace</span>
+                        <span>&gt;</span>
+                        <span>Folder 2</span>
+                        <span>&gt;</span>
+                        <span className='current'>Spreadsheeet 3</span>
+                        <span className='three-dots'>...</span>
+                    </div>
+                </div>
+                <div className="actions-right">
+                    <div className="search">
+                        <input type='text' placeholder='🔍 Search within sheet' />
+                    </div>
+                    <div className="bell">
+                        <span class="badge">2</span>
+                        <img src="/Images/bell.png" class="bell-icon" alt="Notification Bell" />
+                    </div>
+                    <div className="profile-block">
+                        <div className="profile-img">
+                            <img src='/Images/profile_pic.png' alt='Profile Pic' />
+                        </div>
+                        <div className="user-details">
+                            <p className='name'>John Doe</p>
+                            <p className='username'>john.doe...</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
